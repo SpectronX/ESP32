4112cf28b9ffd08960492df4357109dc7434bc29
Samuel Fiifi <algorithmicsamuelx@gmail.com>
2025-06-09 14:14:34 +0000

    Update Station.ino

--- WiFi Setup/Station.ino
+++ WiFi Setup/Station.ino
@@ -3,11 +3,18 @@
 #include <WiFi.h>
 
 void setup(){
-
-  
+  WiFi.mode(WIFI_STA); //Setup of mode
+    WiFi.begin("Wokwi-GUEST", "");
+    Serial.println("Connecting to WiFi");
+    while(WiFi.status() != WL_CONNECTED){
+        Serial.print(".");
+        delay(300);
+    }
+    Serial.println("Network is Up");
+    Serial.print("IP Address: ");
+    Serial.println(WiFi.localIP());
 }
 
 void loop(){
-
-  
+  //Other codes go here
 }
