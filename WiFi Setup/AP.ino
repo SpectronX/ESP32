81a7c35c6a9786e2dad1d0d02a433536284066d3
Samuel Fiifi <algorithmicsamuelx@gmail.com>
2025-06-10 02:23:21 +0000

    Update AP.ino

--- WiFi Setup/AP.ino
+++ WiFi Setup/AP.ino
@@ -1,9 +1,16 @@
 //Code for setting up the ESP32 in Access Point Mode
 
+//Including the required Libraries
+#include <Arduino.h>
+#include <WiFi.h>
+
 void setup(){
+  Serial.begin(115200);
+  WiFi.mode();
 
 
 }
 
 void loop(){
+  //Other Code goes here
 }
