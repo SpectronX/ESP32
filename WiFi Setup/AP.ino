a85b067fef3832f9389b05b1ebf86f04c9da4304
Samuel Fiifi <algorithmicsamuelx@gmail.com>
2025-06-10 02:54:53 +0000

    Update AP.ino

--- WiFi Setup/AP.ino
+++ WiFi Setup/AP.ino
@@ -1,16 +1,25 @@
-//Code for setting up the ESP32 in Access Point Mode
+//Simple code for setting up the ESP32 in Access Point Mode
 
 //Including the required Libraries
 #include <Arduino.h>
 #include <WiFi.h>
 
+const char* ssid = "Access Point";      // Name of the Wi-Fi network to create
+const char* password = "1234";    // Password for the network
+
 void setup(){
   Serial.begin(115200);
-  WiFi.mode();
+  
+  WiFi.mode(WIFI_AP); // Setting up in Acess Point Mode
+  WiFi.softAp(ssid, password); // Start the AP with the given SSID and Password
 
-
+  Serial.println("Access Point Started");
+  Serial.print("Network Name (SSID): ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.softAPIP()); // Show IP assigned to the AP
 }
 
 void loop(){
-  //Other Code goes here
+  //Main Code goes here
 }
